ba5cde807b09cc70c795311503d5064524d80082
rawdydev <dadullahakbary@gmail.com>
2025-01-01 03:15:02 +0330

    added Mock api and data Loader

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import Jobs from '../jobs.json'
+import { useState, useEffect} from 'react'
 import JobListing from './JobListing'
+import Spinner from '../components/Spinner';
 
-const JobListings = () => {
-  const recentJobs = Jobs.slice(0, 3);
+const JobListings = ({isHome = false}) => {
+  // const jobListings = isHome ? Jobs.slice(0, 3) : Jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrls = isHome ? "http://localhost:8000/Jobs?_limit=3" : "http://localhost:8000/Jobs";
+      try {
+        const res = await fetch(apiUrls)
+        const data = await res.json();
+        setJobs(data)
+      } catch (error) {
+        console.log("Failed to fetch the data error:", error)
+      } finally {
+        setTimeout(() => {
+          setLoading(false)  
+        }, 1000);
+        
+      }
+    }
+
+    fetchJobs()
+  }, [])
 
   return (
   <section className="bg-blue-50 px-4 py-10">
     <div className="container-xl lg:container m-auto">
       <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-        Browse Jobs
+        {isHome ? 'Recent Jobs' : 'Browse Jobs' }
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {recentJobs.map((job) => (
-          <JobListing key={job.id} job={job}/>          
-        ))}
-      </div>
+        {loading ? ( <Spinner loading={loading}/> ) : ( 
+          <>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((job) => (
+              <JobListing key={job.id} job={job}/>          
+              ))}
+            </div>
+          </>
+        )}
+        
     </div>
   </section>
   )
--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import JobListings from '../components/JobListings'
 
 const Jobs = () => {
   return (
-    <div>Jobs</div>
+    <section className='bg-blue-50 py-6 px-4'>
+      <JobListings />
+    </section>
   )
 }
 
